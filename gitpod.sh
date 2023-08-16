7c2d45bc8fbec960f0ececbb1ffea8100d78e0bb
Madison Wirtanen <madisonwirtanen@outlook.com>
2023-08-16 13:46:41 +0000

    Update mobile.logo.text links ; delete public

--- gitpod.sh
+++ gitpod.sh
@@ -1,8 +1,4 @@
-mkdir public
-cp -r css images js public
-cp favicon.ico public
 npm i -g geneasy
-geneasy -t index.hbs -o public/index.html links.yml
-cp -r public/* ./
+geneasy -t index.hbs -o index.html links.yml
 npm i -g http-server
-cd public && http-server -p 8000
+http-server -p 8000
